8826124ac11e4746c36029949359de9dd42e6851
Jesse Jeremias <72278@rocteraa-student.nl>
2019-08-29 09:55:40 +0200

    Added minified less files; Removed seed images; Added libraries to project to reduce page load; Added favicon;

--- RentVision/Startup.cs
+++ RentVision/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Piranha;
 using Piranha.AspNetCore.Identity.SQLServer;
+using System.IO;
 
 namespace RentVision
 {
